557783b3c8fbf0c5126e7ed53e78ce3fbe24121d
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2020-10-24 09:28:45 +0200

    feat: setup basic store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { FunctionComponent, useCallback, useReducer } from "react";
+import initialData, { ElementDefinition } from "./data/initialData";
 
-function App() {
-  return;
-}
+const UPDATE_ELEMENT = "UPDATE_ELEMENT";
+type State = ElementDefinition[];
+type Action = {
+  type: typeof UPDATE_ELEMENT;
+  payload: { name: string; value: any };
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case UPDATE_ELEMENT:
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+const App: FunctionComponent = () => {
+  const [state, dispatch] = useReducer(reducer, initialData);
+
+  const onUpdate = useCallback(
+    (payload) => dispatch({ type: UPDATE_ELEMENT, payload }),
+    []
+  );
+
+  return <Renderer data={state} onUpdate={onUpdate} />;
+};
 
 export default App;
