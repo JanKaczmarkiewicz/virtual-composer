7e34cd254fce5fb6f6df59fa7c1cea44f238d13b
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2020-10-24 09:12:20 +0200

    feat: initialData setup

---
+++ src/data/initialData.ts
@@ -0,0 +1,59 @@
+enum Element {
+  SECTION,
+  COLUMN,
+  WIDGET,
+}
+
+type ElementDefinition = {
+  sectionName: string;
+  attributes: Record<string, any>;
+  elementType: Element;
+  content: ElementDefinition[];
+};
+
+const id = () => Math.random().toString();
+
+const cN = (
+  elementType: Element,
+  content: ElementDefinition[],
+  attributes?: Record<string, any>
+): ElementDefinition => ({
+  sectionName: id(),
+  elementType,
+  content,
+  attributes: attributes ? attributes : {},
+});
+
+export default [
+  cN(Element.SECTION, [
+    cN(Element.COLUMN, [
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+    ]),
+  ]),
+  cN(Element.SECTION, [
+    cN(Element.COLUMN, [
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+    ]),
+    cN(Element.COLUMN, [
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+    ]),
+  ]),
+  cN(Element.SECTION, [
+    cN(Element.COLUMN, [
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+    ]),
+    cN(Element.COLUMN, [
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+      cN(Element.WIDGET, []),
+    ]),
+  ]),
+] as ElementDefinition[];
